c5a82ccdb35993d7c7bf543083a9d9d25520ccbd
Sunil Anand2 <anand.sonu@gmail.com>
2020-10-12 07:05:16 +0530

    Netflix Initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hey Sunil, Let 's build Netflix Clone</h1>
+    </div>
+  );
+}
+
+export default App;

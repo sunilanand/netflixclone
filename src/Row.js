bb00a61ff33f58b21329258386fafe20130ce71c
Sunil Anand2 <anand.sonu@gmail.com>
2020-10-26 15:08:03 +0530

    Final

--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 import axios from './axios';
 
 import "./Row.css";
@@ -7,6 +9,8 @@ const base_url = "https://image.tmdb.org/t/p/original";
 
 const Row = ({title, fetchUrl, isLargeRow}) => {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
+    const [opts, setOpts] = useState("");
 
     // A snippet of code which runs based on a specific condition
     useEffect(() => {
@@ -25,6 +29,22 @@ const Row = ({title, fetchUrl, isLargeRow}) => {
         
     }, [fetchUrl])//--- why fetchUrl here => This way we are telling useEffect that we are using this variable from outside the block, and the auto execution is dependent of this variable value change. Now if the value of this variable is updated, the code inside this block will execute again and update the array value.
 
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        }else{
+            setTrailerUrl("");
+            const trailerName = (movie?.name || movie?.title);
+            console.log("Name>>>>>>> ", trailerName);
+            movieTrailer (trailerName)
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    console.log("VVV>",urlParams.get("v"))
+                    setTrailerUrl(urlParams.get("v"));
+                })
+                .catch((error) => console.log(error));
+        }
+    }
 
     //console.log(movies);
     return (
@@ -37,11 +57,14 @@ const Row = ({title, fetchUrl, isLargeRow}) => {
                 {movies.map( movie => (
                     <img
                     key={movie.id}
+                    onClick={() => handleClick(movie)}
                     className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                     src={`${base_url}${isLargeRow?movie.poster_path:movie.backdrop_path}`} alt={movie.name} />
                 ))}
             </div>
             {/* container -> posters */}
+
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
